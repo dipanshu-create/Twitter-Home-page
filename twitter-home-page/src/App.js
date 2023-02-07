8dc9de5cdd00347696fd0d692de86bc870e464cb
dipanshuaswal <dipanshuaswal000@gmail.com>
2023-02-07 16:20:48 +0530

    adding side bar and home

---
+++ twitter-home-page/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>adfasdf</div>
+      <div>adfasdf</div>
+      <div>adfasd</div>
+    </div>
+  );
+}
+
+export default App;
