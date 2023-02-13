341fdb899b023a59fe8e157d985f49ab88b7a0a9
dipanshuaswal <dipanshuaswal000@gmail.com>
2023-02-13 23:48:33 +0530

    css

--- twitter-home-page/src/right_bar.js
+++ twitter-home-page/src/right_bar.js
@@ -8,12 +8,25 @@ export default class Right_bar extends Component {
         <div class="search">
           <input class="input" placeholder="Search Twitter"></input>
         </div>
+
         <div class="box">
           <div class="a">
             <p class="a1">Trends for you</p>
             <p class="a1" id="a12">
               *
             </p>
+          </div>
+          <p class="border"></p>
+
+          <div>
+            <p class="trending">Trending worldwide</p>
+            <p class="hashtag">#BreakingNews</p>
+            <div class="box1">
+              <div class="heading"> space</div>
+              <div class="a12"></div>
+              <div class="a12"></div>
+              <div class="a12"></div>
+            </div>
           </div>
         </div>
       </div>
