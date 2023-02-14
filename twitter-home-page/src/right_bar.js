711157f3ed166b7380c9e4726a0455a3f7100888
dipanshuaswal <dipanshuaswal000@gmail.com>
2023-02-14 12:39:57 +0530

    completed the right side

--- twitter-home-page/src/right_bar.js
+++ twitter-home-page/src/right_bar.js
@@ -22,12 +22,56 @@ export default class Right_bar extends Component {
             <p class="trending">Trending worldwide</p>
             <p class="hashtag">#BreakingNews</p>
             <div class="box1">
-              <div class="heading"> space</div>
-              <div class="a12"></div>
-              <div class="a12"></div>
-              <div class="a12"></div>
+              <div class="heading">
+                space
+                <div class="para">
+                  loner photography improves the discovery of the moon
+                </div>
+              </div>
+
+              <div class="image">
+                <img class="image1" src={require("./download.jpg")}></img>
+              </div>
             </div>
           </div>
+          <p class="people">10,094 people are twitting about this</p>
+          <p class="border"></p>
+
+          <div>
+            <p class="trending">Trending worldwide</p>
+            <p class="hashtag">#WorldNews</p>
+            <p class="number">125k Tweets</p>
+            <p class="people1">5094 people are twitting about this</p>
+            <p class="border"></p>
+          </div>
+
+          <div>
+            <p class="trending">Trending worldwide</p>
+            <p class="hashtag">#BreakingNews</p>
+            <div class="box1">
+              <div class="heading">
+                Animals
+                <div class="para">
+                  These cats are ready for #IntrenationlCatDay
+                </div>
+              </div>
+
+              <div class="image">
+                <img class="image1" src={require("./images.jpg")}></img>
+              </div>
+            </div>
+          </div>
+          <p class="people">10,094 people are twitting about this</p>
+          <p class="border"></p>
+
+          <div>
+            <p class="trending">Trending worldwide</p>
+            <p class="hashtag">#GreatesOfAllTime</p>
+            <p class="number">100k Tweets</p>
+            <p class="people1">4123 people are twitting about this</p>
+            <p class="border"></p>
+          </div>
+          <div class="show">Show more</div>
         </div>
       </div>
     );
