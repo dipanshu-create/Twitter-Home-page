abb9440b4a616bb2aa4e6a524d2967ee4b4c289d
dipanshuaswal <dipanshuaswal000@gmail.com>
2023-02-15 23:38:30 +0530

    start working on home part

--- twitter-home-page/src/home.js
+++ twitter-home-page/src/home.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
+import "./home.css";
 
 export default class Home extends Component {
   render() {
-    return <div>home</div>;
+    return (
+      <div>
+        <div class="ho">
+          <p class="h">Home</p>
+        </div>
+        <p class="border"></p>
+
+        <div>
+          <div class="image"></div>
+          <div class="post"></div>
+        </div>
+      </div>
+    );
   }
 }
